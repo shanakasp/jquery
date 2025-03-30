3df26f5c1de1fbbe1bb2a8b280358e3e20a31a26
Shanaka Prince <shanakaprince@gmail.com>
2025-03-30 20:42:57 +0530

    changed 3 files

---
+++ 02events.js
@@ -0,0 +1,15 @@
+$("#test").click(function () {
+  $(this).hide();
+});
+
+$("#test").mouseout(function () {
+  $(this).hide();
+});
+
+$("#test").mouseover(function () {
+  $(this).hide();
+});
+
+$("#test").keypress(function (e) {
+  $(this).hide();
+});
