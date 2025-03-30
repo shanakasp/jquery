137785d84b15ba0997f4fe59e7887bc05f227258
Shanaka Prince <shanakaprince@gmail.com>
2025-03-30 20:46:21 +0530

    modified 02events.js in .

--- 02events.js
+++ 02events.js
@@ -10,6 +10,17 @@ $("#test").mouseover(function () {
   $(this).hide();
 });
 
+var menuTrigger = false;
+console.log(menuTrigger);
+
 $("#test").keypress(function (e) {
-  $(this).hide();
+  if (e.which == 13) {
+    if (menuTrigger == false) {
+      $("nav").fadeIn(500);
+      menuTrigger = true;
+    } else if (menuTrigger === true) {
+      $("nav").fadeout(500);
+      menuTrigger == false;
+    }
+  }
 });
